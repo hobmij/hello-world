ace650d04abaf63464c34c80d3eac8e1b5928a89
James Bohmbach <hobmij@gmail.com>
2024-10-21 21:27:18 -0500

    Introducing object Component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet' // note that import Greet could be any name 
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
